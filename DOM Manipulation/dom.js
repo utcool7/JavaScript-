d3a122bc065063242b78a9fb983812141dc295f9
utcool7 <31389321+utcool7@users.noreply.github.com>
2021-04-14 14:54:25 +0530

    title and add item styling changed

--- DOM Manipulation/dom.js
+++ DOM Manipulation/dom.js
@@ -1,18 +1,34 @@
-// EXAMINE THE DOCUMENT OBJECT //
+// // EXAMINE THE DOCUMENT OBJECT //
 
-console.dir(document);
-console.log(document.domain);
-console.log(document.URL);
-console.log(document.title);
+// console.dir(document);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
 
-//changing the title of the page
-document.title = 123;
-console.log(document.doctype);
-console.log(document.head);
-console.log(document.body);
-console.log(document.all);
-console.log(document.all[10]);
-// document.all[10].textContent = 'Hello';
-console.log(document.forms[0]);
-console.log(document.links);
-console.log(document.images);
+// //changing the title of the page
+// document.title = 123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// // document.all[10].textContent = 'Hello';
+// console.log(document.forms[0]);
+// console.log(document.links);
+// console.log(document.images);
+
+
+
+#problem 3 
+
+
+//Difference b/w textContent and innerText is
+// innerText pays attention to the styling whereas
+//textContent does not 
+
+var header = document.getElementById('main-header');
+header.style.borderBottom = 'solid 3px #000';
+
+var elements = document.getElementsByClassName('title');
+elements[0].style.fontWeight = 'bold';
+elements[0].style.color = 'green';
